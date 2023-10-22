ab79cea70d6afd70ab655c19cab8fa91af19d699
Chaitanya Sharma <chait8126@gmail.com>
2023-10-22 19:39:02 +0530

    dynamic login using delegates

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -2,35 +2,24 @@
 
 namespace Delegates
 {
-	public delegate void PromoteDelegate(List<Employee> Employees);
-	public delegate void HelloDelegate(string message);
 	public class Program
 	{
 		static void Main(string[] args)
 		{
-			HelloDelegate del = new HelloDelegate(Hello);
-			del("Hello, World");
-
 			List<Employee> Employees = new List<Employee>();
 			Employees.Add(new Employee { ID=1,Experience=3,Salary=10000,Name="Chaitanya"});
 			Employees.Add(new Employee { ID=2,Experience=4,Salary=10000,Name="Kanhaiya"});
 			Employees.Add(new Employee { ID=3,Experience=5,Salary=10000,Name="Charu"});
 			Employees.Add(new Employee { ID=4,Experience=6,Salary=10000,Name="Rishu"});
-			PromoteDelegate pd = new PromoteDelegate(Employee.PromoteEmployee);
-			pd(Employees);
-
 
 			PromotioCriteria pc = new PromotioCriteria(PromoteInSix);
 			BetterExample.BetterPromotion(Employees,pc);
+			BetterExample.BetterPromotion(Employees, (x) => x.Experience >= 5);
 		}
 		public static bool PromoteInSix(Employee emp)
 		{
 			if (emp.Experience >= 6) return true;
 			return false;
-		}
-		public static void Hello(string message)
-		{
-			Console.WriteLine(message);
 		}
 	}
 }
