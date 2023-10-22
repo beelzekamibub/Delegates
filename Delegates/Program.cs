1f6e5f28ac4f6dd0ea43b7f2270460bded7738cb
Chaitanya Sharma <chait8126@gmail.com>
2023-10-22 20:00:52 +0530

    multicast delegate

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -2,8 +2,17 @@
 
 namespace Delegates
 {
+	public delegate void hello();
 	public class Program
 	{
+		public static void Hello1()
+		{
+			Console.WriteLine("Hello1");
+		}
+		public static void Hello2()
+		{
+			Console.WriteLine("Hello2");
+		}
 		static void Main(string[] args)
 		{
 			List<Employee> Employees = new List<Employee>();
@@ -15,11 +24,16 @@ namespace Delegates
 			PromotioCriteria pc = new PromotioCriteria(PromoteInSix);
 			BetterExample.BetterPromotion(Employees,pc);
 			BetterExample.BetterPromotion(Employees, (x) => x.Experience >= 5);
+
+			hello h=new hello(Hello1);
+			h += Hello2;
+			h();
 		}
 		public static bool PromoteInSix(Employee emp)
 		{
 			if (emp.Experience >= 6) return true;
 			return false;
 		}
+		
 	}
 }
