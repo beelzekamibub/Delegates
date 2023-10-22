d6125600d4d1ec42860512659112a4da3cd02b71
Chaitanya Sharma <chait8126@gmail.com>
2023-10-22 16:35:17 +0530

    example 1

---
+++ Delegates/Examples/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates.Examples
+{
+	public class Employee
+	{
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
+        public static void PromoteEmployee(List<Employee> employees)
+        {
+            foreach(Employee employee in employees)
+            {
+                if(employee.Experience>=5)
+                {
+                    Console.WriteLine(employee.Name+" Promoted");
+                    employee.Salary = (int)(employee.Salary * 1.5);
+                }
+            }
+        }
+    }
+}
--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,5 +1,8 @@
-﻿namespace Delegates
+﻿using Delegates.Examples;
+
+namespace Delegates
 {
+	public delegate void PromoteDelegate(List<Employee> Employees);
 	public delegate void HelloDelegate(string message);
 	public class Program
 	{
@@ -7,6 +10,14 @@
 		{
 			HelloDelegate del = new HelloDelegate(Hello);
 			del("Hello, World");
+
+			List<Employee> Employees = new List<Employee>();
+			Employees.Add(new Employee { ID=1,Experience=3,Salary=10000,Name="Chaitanya"});
+			Employees.Add(new Employee { ID=2,Experience=4,Salary=10000,Name="Kanhaiya"});
+			Employees.Add(new Employee { ID=3,Experience=5,Salary=10000,Name="Charu"});
+			Employees.Add(new Employee { ID=4,Experience=6,Salary=10000,Name="Rishu"});
+			PromoteDelegate pd = new PromoteDelegate(Employee.PromoteEmployee);
+			pd(Employees);
 		}
 		public static void Hello(string message)
 		{
